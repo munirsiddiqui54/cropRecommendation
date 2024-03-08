5dc5a518f56773df981ce72375363e595d0831ac
munirsiddiqui54 <122617589+munirsiddiqui54@users.noreply.github.com>
2024-03-08 22:34:31 +0530

    Add files via upload
    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask ,jsonify,request
+import pickle
+import pandas as pd
+import sklearn
+import joblib
+
+app =Flask(__name__)
+
+
+# Load the Random Forest model
+model = joblib.load('RandomForest.pkl')
+
+print()
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data = request.get_json()
+
+    N = data.get('n')
+    P = data.get('p')
+    k = data.get('k')
+    temperature = data.get('t')
+    humidity=data.get('h')
+    pH=data.get('ph')
+    rainfall=data.get("r")
+    crop=model.predict([[N,P,k,temperature,humidity,pH,rainfall]])[0]
+    return jsonify({'crop':crop})
+
+if __name__=='__main__':
+    app.run(port=3000,debug=True)
